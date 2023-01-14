d93e9e1039bab74b5bafd51af0a65e26938573eb
ukrbride4u <ukrbride4u@gmail.com>
2023-01-14 10:26:31 +0200

    branch 4

---
+++ routes/api/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { Wrapper } = require("../../helpers");
+const { validateBody, authenticate } = require("../../middleware");
+const ctrl = require("../../controllers/auth");
+const { schemas } = require("../../models/user");
+
+const router = express.Router();
+router.post(
+  "/signup",
+  validateBody(schemas.registerSchema),
+  Wrapper(ctrl.register)
+);
+
+router.post("/login", validateBody(schemas.loginSchema), Wrapper(ctrl.login));
+
+router.get("/current", authenticate, Wrapper(ctrl.getCurrent));
+
+router.post("/logout", authenticate, Wrapper(ctrl.logout));
+module.exports = router;
